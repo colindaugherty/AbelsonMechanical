ee0d9d1f8802aa5165111b5f81054614d9dc4130
Denise Witt <denise@bigideamediagroup.com>
2016-01-08 12:45:45 -0500

    Started work on users and jobs databases

---
+++ databases.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+db_file = 'abelson.sqlite'
+user = ['username', 'password']
+
+def make_dict(tup_list, fields):
+    return [dict(zip(fields, d)) for d in tup_list]
+
+def connect(db_file):
+    db = sqlite3.connect(db_file)
+    c = db.cursor()
+
+    return (db, c)
+
+def clean_up(db, c):
+    c.close()
+    db.close()
+
+def get_user(username):
+    db, c = connect()
+
+    c.execute("SELECT * FROM user WHERE username=?", (username,))
+    result = c.fetchone()
+    result = make_dict(result, user)
+    clean_up(db, c)
+
+    return result
--- server.py
+++ server.py
@@ -13,7 +13,7 @@ conn = sqlite3.connect('abelson.sqlite')
 c = conn.cursor()
 
 c.execute("""CREATE TABLE IF NOT EXISTS jobs (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(75) NOT NULL UNIQUE, location VARCHAR(20) NOT NULL, description VARCHAR(150) NOT NULL)""")
-c.execute("""CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR(25) NOT NULL UNIQUE, password VARCHAR(50) NOT NULL)""")
+c.execute("""CREATE TABLE IF NOT EXISTS users (username VARCHAR(25) NOT NULL UNIQUE, password VARCHAR(50) NOT NULL)""")
 
 conn.commit()
 conn.close()
@@ -27,7 +27,6 @@ login_manager.init_app(app)
 Flask.secret_key = "random"
 
 #utility functions
-
 def make_dict(tup_list, fields):
     return [dict(zip(fields, d)) for d in tup_list]
 
@@ -36,14 +35,7 @@ class UserNotFoundError(Exception):
     pass
 
 #SQlite Jobs and Locations Code Thing
-conn = sqlite3.connect('abelson.sqlite')
-c = conn.cursor()
-for row in c.execute('SELECT * FROM users'):
-    users.append(row)
-    print(row)
-    print(users)
-conn.commit()
-conn.close()
+
 
 # Simple user class base on UserMixin
 # http://flask-login.readthedocs.org/en/latest/_modules/flask/ext/login.html#UserMixin
