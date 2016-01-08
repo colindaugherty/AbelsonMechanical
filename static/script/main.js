93669bb486dbf07dfd18a6b139fd6b55c430e16f
Denise Witt <denise@bigideamediagroup.com>
2016-01-08 12:53:42 -0500

    Changed it over to faster js

--- static/script/main.js
+++ static/script/main.js
@@ -1,51 +1,29 @@
-var animationControler = (function(){
-  select = $('#careers_edit');
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
 
-  select.on("change", function(e){
-  div = $('#edit_div');
-    div.addClass('fadein');
-  });
+var ABELSONMECHANICAL = (function(){
+
+  function animationController() {
+    select = $('#careers_edit');
+
+    select.on("change", function(e){
+    div = $('#edit_div');
+      div.addClass('fadein');
+    });
+  }
+
+  function init() {
+      animationController();
+  }
+
+  return {
+    init: init
+  };
 })();
 
-var newCareersPost = (function(){
-  newJobName = $('#new_job_name');
-  newJobLoc = $('#new_careers_loc');
-  newJobDesc = $('#new_job_desiption');
-
-  newJobNameValue = newJobName.value;
-  newJobLocValue = newJobLoc.value;
-  newJobDescValue = newJobDesc.value;
-
-  newJob = [newJobNameValue, newJobLocValue, newJobDescValue];
-
-  function PostRequest(){
-
-      e.preventDefault();
-      fetch(newJob, {
-        method: "POST",
-        body: data
-      }).then(fetchHandler).then(responseHandler);
-    }
-
-    function fetchHandler(r) {
-      return r.json();
-    }
-
-    function responseHandler(r) {
-        window.alert("It worked!");
-    }
-
-})();
-
-var updateloc = (function(){
-  var locName = $("#careers_edit"),
-      locAddress = $("#edit_loc_address"),
-      locEmail = $("#edit_loc_email"),
-      locFax = $("#edit_loc_fax"),
-      locPhone = $("#edit_loc_phone");
-
-})();
-
-var updatejob = (function(){
-
-})();
+ready(ABELSONMECHANICAL.init());
