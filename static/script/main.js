8d135c748cc84724938e8416086785fc398d35fa
Garrett Mestel <gmestel98@gmail.com>
2016-01-08 13:06:42 -0500

    add js

--- static/script/main.js
+++ static/script/main.js
@@ -7,30 +7,17 @@ function ready(fn) {
 }
 
 var ABELSONMECHANICAL = (function(){
+    var select, div;
 
-  function animationController() {
+ function animationController() {
     select = $('#careers_edit');
 
     select.on("change", function(e){
-    div = $('#edit_div');
+      div = $('#edit_div');
       div.addClass('fadein');
     });
   }
 
-  function careerEdit(){
-    var data = new FormData(form);
-    $.ajax({type: 'POST',url: '/my/url',data: data});
-  }
-
-  function careerNew(){
-    var data = new FormData(form);
-    $.ajax({type: 'POST',url: '/my/url',data: data});
-  }
-
-  function LocEdit(){
-    var data = new FormData(form);
-    $.ajax({type: 'POST',url: '/my/url',data: data});
-  }
 
   function init() {
       animationController();
