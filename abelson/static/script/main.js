961e653bc204d76ee675314a1e51d7e4a702e6fb
Garrett Mestel <gmestel98@gmail.com>
2016-02-26 14:29:10 -0500

    added the change in the x button when hovering

--- abelson/static/script/main.js
+++ abelson/static/script/main.js
@@ -27,10 +27,22 @@
       toggleClass('hidden', '#menu2');
     }
 
+    const deleat1 = function deleat1 (e) {
+      console.log("hi");
+      e.target.src="/static/images/deletehighlighted.png";
+    }
+
+    const deleat2 = function deleat2 (e) {
+      console.log(e.target);
+      e.target.src="/static/images/delete.png";
+    }
+
     const init = function init () {
       $("body").on("change", "#loc_edit", 'hidden', animationController);
       $("#lines").on("click", menuChanger1);
       $("#menu2pic").on("click", menuChanger1);
+      $(".DeleatA").on("mouseenter", deleat1);
+      $(".DeleatA").on("mouseleave", deleat2);
     };
 
     return {
